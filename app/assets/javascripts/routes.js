fc04d7672378dba986b5de039630a06c21f1c6bb
Savannah Scott <sascott624@gmail.com>
2016-09-01 12:33:40 -0400

    mapped out welcome page

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,15 +1,11 @@
 angular
     .module('app')
     .config(function($urlRouterProvider, $stateProvider) {
-
-        var homeState = {
-            name: 'home',
-            url: '/',
-            templateUrl: 'home/home.html',
-            controller: 'HomeController as vm'
-        }
-        
-        $stateProvider.state(homeState);
-
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'home/home.html',
+          controller: 'HomeController as vm'
+        })
         $urlRouterProvider.otherwise('/');
     });
