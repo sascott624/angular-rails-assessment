a581171c1f7ef1a6a5dc42f83a1f526a3dfaf36f
Savannah Scott <sascott624@gmail.com>
2016-09-01 19:05:23 -0400

    added favoriterecipe join, added relationships to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :favorite_recipes
+  has_many :favorited_by, through: :favorite_recipes, source: :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :recipes
+  has_many :favorite_recipes
+  has_many :favorites, through: :favorite_recipes, source: :recipe
+
 end
