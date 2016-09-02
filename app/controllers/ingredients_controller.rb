e7092ca6fbe83d7c3d14b28a21cf7a23ca20a2f7
Savannah Scott <sascott624@gmail.com>
2016-09-02 09:30:05 -0400

    created ingredients_controller and added resources:ingredients to routes

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,56 @@
+class IngredientsController < ApplicationController
+
+  before_action :ingredient_find, only: [:show, :edit, :destroy]
+
+  def index
+    @ingredients = Ingredient.all
+    render json: @ingredients
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      render json: @ingredient, status: 201
+    else
+      @ingredient.errors.add("ingredient error")
+      render :new
+    end
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def show
+    render json: @ingredient
+  end
+
+  def update
+    @ingredient.update(ingredient_params)
+    if @ingredient.save
+      render json: @ingredient
+    else
+      @ingredient.errors.add("ingredient error")
+      render :edit
+    end
+  end
+
+  def destroy
+    @ingredient.destroy
+    redirect_to ingredients_path
+  end
+
+  def edit
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :inventory)
+  end
+
+  def ingredient_find
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'angular_app#index'
   resources :recipes
+  resources :ingredients
   devise_for :users
 
 end
