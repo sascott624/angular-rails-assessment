34521c80cf4112c1631ce254cbff1dfa6e4556e6
Savannah Scott <sascott624@gmail.com>
2016-09-02 08:34:25 -0400

    added search functionality to recipes

--- app/assets/javascripts/recipes/recipes.controller.js
+++ app/assets/javascripts/recipes/recipes.controller.js
@@ -1,7 +1,14 @@
-function RecipesController(recipes) {
+function RecipesController(recipes, $filter) {
     var vm = this;
     vm.recipes = recipes.data;
     // console.log(recipes.data);
+    vm.search = '';
+
+    vm.refilter = function(){
+      vm.filteredRecipes = $filter('filter')(vm.recipes, vm.search);
+    };
+
+    vm.refilter();
 }
 
 angular
