1a5cec34a174021497f8bb0ec091bead4b67d601
Savannah Scott <sascott624@gmail.com>
2016-09-01 20:00:06 -0400

    added recipes controller.rb json responsive

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,56 @@
+class RecipesController < ApplicationController
+
+  before_action :recipe_find, only: [:show, :edit, :destroy]
+
+  def index
+    @recipes = Recipe.all
+    render json: @recipes
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      render json: @recipe, status: 201
+    else
+      @recipe.errors.add("recipe error")
+      render :new
+    end
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    render json: @recipe
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    if @recipe.save
+      render json: @recipe
+    else
+      @recipe.errors.add("recipe error")
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  def edit
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
+
+  def recipe_find
+    @recipe = Recipe.find(params[:id])
+  end
+
+end
