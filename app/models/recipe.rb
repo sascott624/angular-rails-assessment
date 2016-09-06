ffd8d163d57ee154e068578b32fbf6932d432fe2
Savannah Scott <sascott624@gmail.com>
2016-09-06 13:35:59 -0400

    updating recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,10 +3,10 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
 
   def ingredient_ids=(ingredients)
-  ingredients.each do |i|
-    ingredient = Ingredient.where(id: i)
-    self.ingredients << ingredient
+    ingredients.each do |i|
+      ingredient = Ingredient.where(id: i)
+      self.ingredients << ingredient
+    end
   end
-end
 
 end
