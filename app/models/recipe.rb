5011006643221f0a0bd25d38c015a889c58865f6
Savannah Scott <sascott624@gmail.com>
2016-09-06 13:42:32 -0400

    enable recipes to accepted nested attrs for recipe_ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,11 +2,13 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
-  def ingredient_ids=(ingredients)
-    ingredients.each do |i|
-      ingredient = Ingredient.where(id: i)
-      self.ingredients << ingredient
-    end
-  end
+  accepts_nested_attributes_for :recipe_ingredients
+
+  # def ingredient_ids=(ingredients)
+  #   ingredients.each do |i|
+  #     ingredient = Ingredient.where(id: i)
+  #     self.ingredients << ingredient
+  #   end
+  # end
 
 end
