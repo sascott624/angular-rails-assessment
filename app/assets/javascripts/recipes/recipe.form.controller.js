4c109166867a729d129989e1956434760bf6bfdd
Savannah Scott <sascott624@gmail.com>
2016-09-06 10:56:58 -0400

    create a new recipe with ingredients

--- app/assets/javascripts/recipes/recipe.form.controller.js
+++ app/assets/javascripts/recipes/recipe.form.controller.js
@@ -1,4 +1,4 @@
-function RecipeFormController(ingredients, RecipeService, $filter,$state){
+function RecipeFormController($scope, ingredients, RecipeService, $filter, $state){
   var vm = this;
 
   vm.ingredients = ingredients.data;
@@ -15,27 +15,15 @@ function RecipeFormController(ingredients, RecipeService, $filter,$state){
     return $filter('filter')(vm.ingredients, { selected: true })
   };
 
-  vm.alert = function(){
-    console.log(vm.selectedIngredients());
-  }
-
-
   vm.createRecipe = function(){
+    vm.recipe.ingredient_ids = vm.selectedIngredients().map(function(ing){
+      return ing.id;
+    });
     RecipeService.postRecipe(vm.recipe).then(function(response){
       console.log(response);
     })
   }
 
-
-  vm.addIngredient = function(){
-    vm.recipe_ingredients.push({quantity: '',
-    measurement: '',
-    ingredient: {
-      id: '',
-      name: "",
-      food_group: ''
-    }});
-  };
 };
 
 angular
